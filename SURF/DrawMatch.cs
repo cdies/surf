266190ae788be9e45075d334df1cd8d89bdd336d
cdies <qwerty33340@mail.ru>
2015-06-08 20:35:02 +0300

    SURF detector or first tab

---
+++ SURF/DrawMatch.cs
@@ -0,0 +1,118 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2013 by EMGU. All rights reserved.       
+//----------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace SURF
+{
+    public static class DrawMatches
+    {
+        public static void FindMatch(Image<Bgr, Byte> modelImage, Image<Bgr, byte> observedImage,  
+            out VectorOfKeyPoint modelKeyPoints, out VectorOfKeyPoint observedKeyPoints, 
+            out Matrix<int> indices, out Matrix<byte> mask, out HomographyMatrix homography)
+        {
+            int k = 2;
+            double uniquenessThreshold = 0.8;
+            SURFDetector surfCPU = new SURFDetector(500, false);
+            homography = null;
+
+
+            // Вычисление ключевой точки(Detect) и сразу дескриптора(локальной особенности, Compute) для model image
+            modelKeyPoints = new VectorOfKeyPoint();
+            Image<Gray, Byte> cpuModelImage = modelImage.Convert<Gray, byte>();
+            Matrix<float> modelDescriptors = surfCPU.DetectAndCompute(cpuModelImage, null, modelKeyPoints);
+
+            // Вычисление ключевой точки и сразу дескриптора(локальной особенности) для observe image
+            observedKeyPoints = new VectorOfKeyPoint();
+            Image<Gray, Byte> cpuObservedImage = observedImage.Convert<Gray, byte>();
+            Matrix<float> observedDescriptors = surfCPU.DetectAndCompute(cpuObservedImage, null, observedKeyPoints);
+
+            // Сравнение дескрипторов
+            BruteForceMatcher<float> matcher = new BruteForceMatcher<float>(DistanceType.L2);
+            matcher.Add(modelDescriptors);
+            indices = new Matrix<int>(observedDescriptors.Rows, k);
+            using (Matrix<float> dist = new Matrix<float>(observedDescriptors.Rows, k))
+            {
+                matcher.KnnMatch(observedDescriptors, indices, dist, k, null);
+                mask = new Matrix<byte>(dist.Rows, 1);
+                mask.SetValue(255);
+                Features2DToolbox.VoteForUniqueness(dist, uniquenessThreshold, mask);
+            }
+
+            // Сопоставление изображений (гомография)
+            int nonZeroCount = CvInvoke.cvCountNonZero(mask);
+            if (nonZeroCount >= 4)
+            {
+                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, 
+                    observedKeyPoints, indices, mask, 1.5, 20);
+                if (nonZeroCount >= 4)
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, 
+                        observedKeyPoints, indices, mask, 2);
+            }         
+        }
+
+        /// <summary>
+        /// Draw the model image and observed image, the matched features and homography projection.
+        /// </summary>
+        /// <param name="modelImage">The model image</param>
+        /// <param name="observedImage">The observed image</param>
+        /// <returns>The model image and observed image, the matched features and homography projection.</returns>
+        public static HomographyMatrix Draw(Image<Bgr, Byte> modelImage, Image<Bgr, byte> observedImage)
+        {
+            HomographyMatrix homography;
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            Matrix<int> indices;
+            Matrix<byte> mask;
+
+            FindMatch(modelImage, observedImage,  out modelKeyPoints, out observedKeyPoints, out indices, out mask, out homography);
+
+            //Draw the matched keypoints
+           //Image<Bgr, Byte> result = Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+           //   indices, new Bgr(255, 0, 0), new Bgr(0, 255, 0), mask, Features2DToolbox.KeypointDrawType.DEFAULT);
+
+            return homography;
+        }
+
+        public static Image<Bgr, Byte> SingleDraw(Image<Bgr, Byte> image)
+        {
+            SURFDetector surfCPU = new SURFDetector(500, false);
+
+            // Вычисление ключевой точки(Detect) и сразу дескриптора(локальной особенности, Compute) для model image
+            VectorOfKeyPoint keyPoints = new VectorOfKeyPoint();
+            Image<Gray, Byte> cpuImage = image.Convert<Gray, byte>();
+            Matrix<float> Descriptors = surfCPU.DetectAndCompute(cpuImage, null, keyPoints);
+            return Features2DToolbox.DrawKeypoints(image, keyPoints, new Bgr(255, 0, 0), Features2DToolbox.KeypointDrawType.DEFAULT);
+        }
+
+        public static Image<Bgr, Byte> DrawRectangle(Image<Bgr, Byte> modelImage, Image<Bgr, Byte> observedImage, HomographyMatrix homography, Bgr color)
+        {
+            #region draw the projected region on the image
+            if (homography != null)
+            {  //draw a rectangle along the projected model
+                Rectangle rect = modelImage.ROI;
+                PointF[] pts = new PointF[] { 
+               new PointF(rect.Left, rect.Bottom),
+               new PointF(rect.Right, rect.Bottom),
+               new PointF(rect.Right, rect.Top),
+               new PointF(rect.Left, rect.Top)};
+                homography.ProjectPoints(pts);
+
+                observedImage.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, color, 2);
+            }
+            #endregion
+
+            return observedImage;
+        }
+
+    }
+}
---
+++ SURF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SURF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Image<Bgr, Byte> SURF_image;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ////////////////////////////////////////////////////////////////////////// вкладка SURF детектор
+        // Добавление изображения для SURF детектора
+        private void btn_add_surf_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txt_add_surf.Text = openFileDialog.FileName;
+            SURF_image = new Image<Bgr, byte>(txt_add_surf.Text);
+            img_SURF.Source = BitmapSourceConvert.ToBitmapSource(SURF_image);
+        }
+        // Определение локальных особенностей
+        private void btn_surf_Click(object sender, RoutedEventArgs e)
+        {
+            if (SURF_image == null)
+                MessageBox.Show("Укажите изображение!");
+            else
+                img_SURF.Source = BitmapSourceConvert.ToBitmapSource(DrawMatches.SingleDraw(SURF_image));                
+        }
+        // Очистка локальных особенностей
+        private void btn_clear_surf_Click(object sender, RoutedEventArgs e)
+        {
+            if (SURF_image == null)
+                MessageBox.Show("Укажите изображение!");
+            img_SURF.Source = BitmapSourceConvert.ToBitmapSource(SURF_image);
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////
+
+
+    }
+}

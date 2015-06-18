5dcb127752363a45a83511cb8f4068dbf0a32acb
cdies <qwerty33340@mail.ru>
2015-06-18 07:08:47 +0300

    Very last release

--- SURF/DrawMatch.cs
+++ SURF/DrawMatch.cs
@@ -1,7 +1,4 @@
-﻿//----------------------------------------------------------------------------
-//  Copyright (C) 2004-2013 by EMGU. All rights reserved.       
-//----------------------------------------------------------------------------
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
